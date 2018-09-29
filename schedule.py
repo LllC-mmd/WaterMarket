8f9c4df61d5bf9b09e37e3240cc1cbb21fdb6b8a
LllC-mmd <951785875@qq.com>
2018-09-29 19:52:05 +0800

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -7,20 +7,25 @@ class MarketActivation(SimultaneousActivation):
     def __init__(self, model):
         super().__init__(model)
 
+    def agent_count(self):
+        self.num = len(self.agents)
+
+    def benefit(self, p_matrix, a_matrix):
+        for i in range(0, self.num):
+            self.agents[i].benefit_table(p_matrix[i], a_matrix[i])
+
     def learn_d(self, p_matrix):
-        total_cost = np.sum(p_matrix)
-        non_zero = np.nonzero(p_matrix)[0].shape[0]
-        t = len(self.agents)
-        if total_cost != 0:
-            average_cost = total_cost / non_zero
-            for i in range(0, t):
+        price_sum = np.sum(p_matrix)
+        non_zero = np.nonzero(p_matrix)[0].shape[0]  # get the total number of non-zeros
+
+        if non_zero != 0:
+            price_avg = price_sum / non_zero
+            for i in range(0, self.num):
                 z = np.array(p_matrix[i])
                 if np.sum(z) > 0:
-                    tau = np.min(z[z>0])
-                    self.agents[i].learn(tau)
+                    self.agents[i].learn()  # learn the outflow, water use and outflow to maximize the benefit
                 else:
-                    tau = average_cost
-                    self.agents[i].learn(tau)
-        else:
-            for i in range(0, t):
+                    self.agents[i].learn_price(price_avg)  # only learn the price
+        else:  # No transaction occurs in the market
+            for i in range(0, self.num):
                 self.agents[i].learn_by_random()
